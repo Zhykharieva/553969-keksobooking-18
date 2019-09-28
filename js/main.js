6e5b8479c7285e031fdd46221e6df9008b5521e1
zhykharieva <zhykharieva@gmail.com>
2019-09-28 16:43:40 +0300

    did 6-th task

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 var TYPE_OF_ACCOMONDATION = ['palace', 'flat', 'house', 'bungalo'];
 var TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var START_COORD = 130;
-var FINISH_COORD = 630;
+var MIN_Y = 130;
+var MAX_Y = 630;
 
 var getRandomBetween = function (min, max) {
   min = Math.ceil(min);
@@ -29,7 +29,7 @@ var createNumberedList = function (count, prefix, suffix) {
 
 var TITLES = createNumberedList(5, 'title', '');
 var DESCRIPTION = createNumberedList(8, 'description', '');
-var PHOTOS = createNumberedList(8, 'http://o0.github.io/assets/images/tokyo/hotel', '.jpg');
+var PHOTOS = createNumberedList(3, 'http://o0.github.io/assets/images/tokyo/hotel', '.jpg');
 var AVATARS = createNumberedList(8, 'img/avatars/user0', '.png');
 
 var getRandomElement = function (arr) {
@@ -41,7 +41,7 @@ var ROOMS = generateRandomNumberArray(4, 5);
 var PRICE = generateRandomNumberArray(10, 9890);
 
 var getRandomSlice = function (arr) {
-  return arr.slice(getRandomElement(arr));
+  return arr.slice(getRandomBetween(0, arr.length - 1));
 };
 
 var spliceRandomElem = function (arr) {
@@ -51,8 +51,15 @@ var spliceRandomElem = function (arr) {
 var getLocation = function () {
   return {
     x: getRandomBetween(0, MAP.offsetWidth),
-    y: getRandomBetween(START_COORD, FINISH_COORD),
+    y: getRandomBetween(MIN_Y, MAX_Y),
   };
+};
+
+var typeOfHousingListMap = {
+  'flat': 'Квартира',
+  'bungalo': 'Бунгало',
+  'house': 'Дом',
+  'palace': 'Дворец',
 };
 
 var getOffer = function (currentLocation) {
@@ -90,6 +97,7 @@ var createPinsData = function (number) {
 
   return pins;
 };
+
 var MAP = document.querySelector('.map');
 MAP.classList.remove('map--faded');
 var TEMPLATE_PIN = document.querySelector('#pin')
@@ -124,3 +132,57 @@ var renderPins = function () {
 };
 
 renderPins();
+
+var similarListElement = MAP.querySelector('.map__filters-container');
+
+var similarElemTemplate = document.querySelector('#card')
+    .content
+    .querySelector('.map__card');
+
+var renderPhotos = function (node, ads) {
+  var photoCardElem = node.querySelector('.popup__photos');
+  var fragment = document.createDocumentFragment();
+
+  ads.offer.photos.forEach(function (elem) {
+    var picture = document.createElement('img');
+    picture.className = 'popup__photo';
+    picture.src = elem;
+    picture.width = 45;
+    picture.height = 40;
+    fragment.appendChild(picture);
+
+  });
+  photoCardElem.appendChild(fragment);
+
+};
+
+var renderFeatures = function (elem, ads) {
+  var featuresElem = elem.querySelector('.popup__features');
+  var featuresCont = '';
+  ads.offer.features.forEach(function (feature) {
+    featuresCont += '<li class="popup__feature popup__feature--' + feature + '" ></li>';
+  });
+  featuresElem.innerHTML = featuresCont;
+};
+
+
+var renderCard = function () {
+  var pinsArray = createPinsData(8);
+  var pinsElem = pinsArray[0];
+  var cardElement = similarElemTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = pinsElem.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = pinsElem.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = pinsElem.offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = typeOfHousingListMap[pinsElem.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = pinsElem.offer.rooms + ' комнаты для ' + pinsElem.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time ').textContent = 'Заезд после ' + pinsElem.offer.checkin + ', выезд до ' + pinsElem.offer.checkout + '.';
+  renderFeatures(cardElement, pinsElem);
+  cardElement.querySelector('.popup__description').textContent = pinsElem.offer.description;
+  cardElement.querySelector('.popup__photos').innerHTML = '';
+  renderPhotos(cardElement, pinsElem);
+  return cardElement;
+};
+
+var fragment = document.createDocumentFragment();
+fragment.appendChild(renderCard());
+MAP.insertBefore(fragment, similarListElement);
